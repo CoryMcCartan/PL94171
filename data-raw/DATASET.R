311c386ee459ce7950fc9daca30bf1d228fea1f1
Christopher Kenny <ctk44@cornell.edu>
2021-03-31 17:27:16 -0400

    sumlev

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -10,3 +10,9 @@ names(fips_to_state) = fips$fips
 
 usethis::use_data(abbr_to_fips, state_to_fips, fips_to_state,
                   internal=TRUE, overwrite=TRUE, version=3)
+
+# summary levels ----
+sumlev <- readr::read_csv(file = 'data-raw/sum_level.csv',
+                          col_names = c('SUMLEV', 'SUMLEV_description'))
+usethis::use_data(sumlev)
+
