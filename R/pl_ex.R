de9d470bcb95c845cf0d06410de7fcd2204c5272
Christopher Kenny <ctk44@cornell.edu>
2021-05-30 22:08:20 -0400

    example data file + more unit tests

---
+++ R/pl_ex.R
@@ -0,0 +1,18 @@
+#' PL Example File
+#'
+#' This data contains a subset of the 2020 prototype PL data
+#'
+#' @name pl_ex
+#' @usage data("pl_ex")
+#' @format list of tibbles containing the four PL files.
+#' \describe{
+#' \item{\code{00001}}{Tables P1 and P2}
+#' \item{\code{00002}}{Tables P3, P4, and H1}
+#' \item{\code{00003}}{Table P5}
+#' \item{\code{geo}}{geographic header file}
+#' }
+#'
+#' @concept data
+#' @examples
+#' data(pl_ex)
+NULL
---
+++ tests/testthat/setup.R
@@ -0,0 +1,1 @@
+data(pl_ex)
