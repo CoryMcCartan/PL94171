7294d4fb6724c428e32735e2648b1c8aa595dce6
Christopher Kenny <ctk44@cornell.edu>
2025-07-23 17:52:08 -0400

    ftp status 350 instead of http 200

--- tests/testthat/test-urls.R
+++ tests/testthat/test-urls.R
@@ -1,9 +1,9 @@
 skip_on_cran()
 
 test_that("PL URLs are generated correctly", {
-    wa10_url = "https://www2.census.gov/census_2010/01-Redistricting_File--PL_94-171/Washington/wa2010.pl.zip"
+    wa10_url = "ftp://ftp2.census.gov/census_2010/01-Redistricting_File--PL_94-171/Washington/wa2010.pl.zip"
     expect_equal(pl_url("WA", 2010), wa10_url)
-    expect_true(httr::HEAD(pl_url("WA", 2000)[3])$all_headers[[1]]$status == 200)
-    expect_true(httr::HEAD(pl_url("DC", 2010))$all_headers[[1]]$status == 200)
-    expect_true(httr::HEAD(pl_url("IA", 2020))$all_headers[[1]]$status == 200)
+    expect_true(attr(curlGetHeaders(pl_url("WA", 2000)[3]), 'status') == 350)
+    expect_true(attr(curlGetHeaders(pl_url("DC", 2010)), 'status') == 350)
+    expect_true(attr(curlGetHeaders(pl_url("IA", 2020)), 'status') == 350)
 })
